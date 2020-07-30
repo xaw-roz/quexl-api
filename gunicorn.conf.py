7c95a359c987e876cf0f947e91c5d5cd970379d9
Saroj Shrestha <sarojs@nirc.com.np>
2020-07-30 22:34:38 +0545

    Made minor fixes.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,26 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ddc1455b6da2a565d1ffaa3a3193ad7e4320e53be35b57c993583996a7e14b6d
-size 695
+# file gunicorn.conf.py
+# coding=utf-8
+# Reference: https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
+import os
+import multiprocessing
+
+_ROOT = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..'))
+_VAR = os.path.join(_ROOT, 'var')
+_ETC = os.path.join(_ROOT, 'etc')
+
+loglevel = 'info'
+# errorlog = os.path.join(_VAR, 'log/api-error.log')
+# accesslog = os.path.join(_VAR, 'log/api-access.log')
+errorlog = "-"
+accesslog = "-"
+
+# bind = 'unix:%s' % os.path.join(_VAR, 'run/gunicorn.sock')
+bind = '0.0.0.0:5000'
+# workers = 3
+workers = multiprocessing.cpu_count() * 2 + 1
+
+timeout = 3 * 60  # 3 minutes
+keepalive = 24 * 60 * 60  # 1 day
+
+capture_output = True
