94b1f8024e9858575ba2821993f013a47f30d9dd
Saroj Shrestha <sarojs@nirc.com.np>
2021-01-18 23:33:12 +0545

    made changes for local

--- app.py
+++ app.py
@@ -1065,7 +1065,7 @@ def graphPlot ():
 
 # if __name__ == "__main__":
 #     app.run(host='0.0.0.0')
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
