9e50586372d7a44f0e3fba43cd7255dc05c9ec21
Saroj Shrestha <sarojs@nirc.com.np>
2021-01-18 23:03:02 +0545

    gasdxx

--- app.py
+++ app.py
@@ -1063,8 +1063,8 @@ def graphPlot ():
     #         data='The input was invalid',
     #     )
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
