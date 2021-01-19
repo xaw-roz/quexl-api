7ad3634db9632d77c00d6342ff1f87fbda5d5895
Saroj Shrestha <sarojs@nirc.com.np>
2021-01-19 19:50:19 +0545

    made changes for proddee

--- apiserver.py
+++ apiserver.py
@@ -1063,9 +1063,9 @@ def graphPlot ():
     #         data='The input was invalid',
     #     )
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
